8e7f695744148cda62bd10036d7d6b7485d265df
Richard HEYNER <richard.heyner@cgi.com>
2025-02-01 19:33:02 +0100

    Add Jacoco + remove sonar.bat.

--- sonar.bat
+++
@@ -1,4 +0,0 @@
-mvn clean verify sonar:sonar -Dsonar.projectKey=RangerFichier
-                             -Dsonar.projectName='RangerFichier'
-                             -Dsonar.host.url=http://localhost:9000
-                             -Dsonar.token=sqb_999ece0135133924ad41fe36a09876d5b3778fa7
